31efa48eef725e4b40186543cf352deb556d4ecb
J.Junquera <javier.junquera.sanchez@gmail.com>
2018-03-04 20:06:44 +0100

    Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc *.c -lssl -o totp
--- totp.c
+++ totp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <strings.h>
 
+#include <openssl/sha.h>
+
 #define sha_block_size 64
 #define OPAD "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
 #define IPAD "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
@@ -13,7 +15,7 @@ int max(int a, int b){
     }
 }
 
-void xor(char** c, char* a, char* b) {
+void str_xor(char** c, char* a, char* b) {
     
     int lenA = strlen(a);
     int lenB = strlen(b);
@@ -28,13 +30,13 @@ void xor(char** c, char* a, char* b) {
     for(i = lenC; i >= 0; i--){
         int itAux = lenC - i;
         if(lenA < i){
-            aaux[lenC - i] = '\x00';
+            aaux[itAux] = '\x00';
         } else {
-            aaux[lenC - i] = a[itAux];
+            aaux[itAux] = a[itAux];
         }
 
         if(lenB < i){
-            baux[lenC - i] = '\x00';
+            baux[itAux] = '\x00';
         } else {
             baux[itAux] = b[itAux];
         }
@@ -43,6 +45,13 @@ void xor(char** c, char* a, char* b) {
     for(i = 0; i < lenC; i++){
         *(*c + i) = aaux[i] ^ baux[i];
     }
+}
+
+
+void sha1(char** output, char* input){
+    int lenI = strlen(input);
+
+    SHA1(input, lenI, output); 
 }
 
 void hmac(char** res, char* K, char* m){
@@ -54,11 +63,16 @@ void main(){
     char* b= "def";
     char* c;
 
-    xor(&c, a, b);
+    str_xor(&c, a, b);
 
     printf("%s\n", c);
     
-    xor(&a, c, b);
+    str_xor(&a, c, b);
    
     printf("%s\n", a);
+
+    sha1(&c, a);
+    
+    printf("%02x\n", c);
+
 }
