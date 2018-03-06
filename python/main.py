6984b9b2ade1881da81ea3110ae90532defe4642
J.Junquera <javier.junquera.sanchez@gmail.com>
2018-03-06 18:19:29 +0100

    Minor refactor and TODOs

--- python/main.py
+++ python/main.py
@@ -1,28 +1,11 @@
 import totp
 import time
 
-def get_time(slot_size):
-    t = int(time.time())
-    remainder = t % slot_size
-
-    t -= remainder
-
-    return t
-
-def next_slot_at(slot_size):
-    t = int(time.time())
-    remainder = t % slot_size
-
-    return slot_size - remainder
-
-
 def bytes2int(b):
     return int.from_bytes(b, byteorder='big', signed=False)
 
-forbidden = [8080, 8081]
-
 # 1 < n < 10
-def gen_ports(val, n=3):
+def gen_ports(val, n=3, forbidden=[]):
     values = []
     for i in range(n):
         aux = bytes2int(val[2*i:(2*i)+2])
@@ -33,29 +16,67 @@ def gen_ports(val, n=3):
         values.append(aux)
     return values
 
-class ConfPorts():
+# TODO Threading & sockets!
+# import threading, socket
 
-    ports = []
+class TocTocPorts():
 
-    def __init__(self):
-        pass
+    def __init__(self, secret, n_ports=4, slot=30, forbidden=[]):
 
-    def put(self, p):
-        if len(self.ports) >= 3:
-            self.ports = self.ports[1:]
-        self.ports.append(p)
+        self._secret = secret
+        self._slot = slot
+        self._forbidden = forbidden
+
+        if n_ports > 20:
+            raise "Error, max ports: %d" % 20
+
+        self._n_ports = n_ports
+
+        ports = self.get_all()
+
+        self._p = ports['p']
+        self._a = ports['a']
+        self._n = ports['n']
+
+        # time.sleep(ns)
+
+    def next(self):
+        t = int(time.time())
+        remainder = t % self._slot
+
+        return self._slot - remainder
+
+
+    def last(self):
+        t = int(time.time())
+        remainder = t % self._slot
+
+        return t - remainder
 
     def get_all(self):
-        return self.ports
+
+        tc = self.last()
+        tcp = tc - self._slot
+        tcn = tc + self._slot
+
+        valp = totp.totp(self._secret, tcp)
+        vala = totp.totp(self._secret, tc)
+        valn = totp.totp(self._secret, tcn)
+
+        portsp = gen_ports(valp, n=self._n_ports, forbidden=self._forbidden)
+        portsa = gen_ports(vala, n=self._n_ports, forbidden=self._forbidden)
+        portsn = gen_ports(valn, n=self._n_ports, forbidden=self._forbidden)
+
+        return {'p': portsp, 'a': portsa, 'n': portsn}
 
     def get_prev(self):
-        return self.ports[0]
+        return self.get_all['p']
 
     def get_actual(self):
-        return self.ports[1]
+        return self.get_all['a']
 
     def get_next(self):
-        return self.ports[2]
+        return self.get_all['n']
 
     def __str__(self):
         res = ''
@@ -64,17 +85,20 @@ class ConfPorts():
         res += ("-" * len(banner))
         res += "\n"
 
-        p = self.ports[0]
-        a = self.ports[1]
-        n = self.ports[2]
+        ports = self.get_all()
+        p = ports['p']
+        a = ports['a']
+        n = ports['n']
 
         for port in range(len(p)):
             res += ("%d\t%d\t\t%d\t\t%d\n" % (port, p[port], a[port], n[port]))
         res += ("-" * len(banner))
-        res += "\n\n"
+        res += "\n"
 
         return res
 
+# TODO https://github.com/ldx/python-iptables
+# TODO https://docs.python.org/3/library/argparse.html
 def main():
 
     slot = 30
@@ -83,36 +107,13 @@ def main():
 
     print("Secret: %s" % secret)
 
-    ports = ConfPorts()
-
-    tc = get_time(slot)
-    tcp = tc - slot
-    tcn = tc + slot
-
-    valp = totp.totp(secret, tcp)
-    vala = totp.totp(secret, tc)
-    valn = totp.totp(secret, tcn)
-
-    portsp = gen_ports(valp, n=4)
-    portsa = gen_ports(vala, n=4)
-    portsn = gen_ports(valn, n=4)
-
-    ports.put(portsp)
-    ports.put(portsa)
-    ports.put(portsn)
+    ports = TocTocPorts(secret, slot)
 
     print(ports)
 
-    ns = next_slot_at(slot)
-    print("Next slot at %ds..." % ns)
-    time.sleep(ns)
+    time.sleep(ports.next())
 
     while 1:
-        tcn = get_time(slot) + slot
-        valn = totp.totp(secret, tcn)
-        portsn = gen_ports(valn, n=4)
-        ports.put(portsn)
-
         print(ports)
 
         time.sleep(slot)
