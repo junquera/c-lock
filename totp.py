943c6bd5ff9090097fe430b7593b209f728dbba8
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-01-03 11:16:04 +0100

    RFC Compliance

--- totp.py
+++ totp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from hashlib import sha256 as hash_alg
+# from hashlib import sha256 as hash_alg
+from hashlib import sha1 as hash_alg
 import uuid
+import codecs
 
 block_size = hash_alg().block_size
 
@@ -34,8 +36,9 @@ def xor(a, b):
 
     return c
 
+# RFC 2104 compliant
 def hmac(K, m):
-    # Rewritten according to RFC 2104
+
     if len(K) > block_size:
         k = hash_alg(K).digest()
     else:
@@ -56,14 +59,37 @@ def hmac(K, m):
     return outer.digest()
 
 
-def hotp(K, C):
-    return hmac(K.encode(), C.encode())
+def otp(secret, moving_factor):
 
+    # Bytes of hexadecimal string
+    s = str2hexs(secret)
+    s = codecs.decode(s, 'hex')
+
+    c = "%016x" % moving_factor
+    c = codecs.decode(c, 'hex')
+
+    r = hmac(s, c)
+
+    offset = r[-1] & 0xf
+    code = (r[offset] & 0x7f) << 24
+    code = code | ((r[offset + 1] & 0xff) << 16)
+    code = code | ((r[offset + 2] & 0xff) << 8)
+    code = code | ((r[offset + 3] & 0xff))
+
+    res = code % 1e10
+
+    return int(res)
+
+def hotp(K, I):
+    res = otp(K, I) % 1e6
+    return int(res)
+
+# TC = Time in seconds
 def totp(K, TC):
-    tcs = int_2_str(TC)
-    res = hotp(K, tcs)
-    return res
 
+    t = int(TC/30)
+    res = otp(K, t) % 1e8
+    return int(res)
 
 def int_2_str(i):
 
@@ -76,3 +102,29 @@ def int_2_str(i):
 
 def gen_secret():
     return "%s%s" % (hash_alg(uuid.uuid4().bytes).hexdigest(), hash_alg(uuid.uuid4().bytes).hexdigest())
+
+
+def bytes2hexs(b):
+    return codecs.encode(b, "hex")
+
+def str2hexs(s):
+    res = codecs.encode(s)
+    res = bytes2hexs(res)
+    return res
+
+
+if __name__ == '__main__':
+    K = 0x3132333435363738393031323334353637383930
+    S = codecs.decode(codecs.decode("%x" % K, "hex"))
+
+    print("secret: ", S)
+
+    # OTP TEST
+    print("otp: ", otp(S, 0))
+
+    # HOTP TEST
+    print("hotp: ", hotp(S, 0))
+
+    # TOTP TEST
+    t = 59
+    print("totp: ", totp(S, t))
