67357c41dfa817db8e4d4313dd11820400cca7d0
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-03-21 23:11:28 +0100

    First feature commit

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
 
+
+def requirements():
+    with open('requirements.txt') as f:
+        raw = f.read()
+
+    return [dependency for dependency in raw.split('\n') if len(dependency)]
+
+
 setup(
     name="clock",
-    tests_require=["pytest"],
+    version="0.0.7",
+    author="Javier Junquera Sánchez",
+    author_email="javier@junquera.xyz",
     packages=find_packages(),
+    url="https://gitlab.com/junquera/c-lock",
+    license="LICENSE.txt",
+    install_requires=requirements(),
+    tests_require=["pytest"],
     entry_points=dict(
         console_scripts= [
             'c-lockd=clock.cli.server:main',
