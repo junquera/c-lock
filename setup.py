490a8190bed91ae37712637ec4cbb65dc63d66b0
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-03-29 23:27:19 +0100

    Minor changes

--- setup.py
+++ setup.py
@@ -8,14 +8,22 @@ def requirements():
     return [dependency for dependency in raw.split('\n') if len(dependency)]
 
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
-    name="clock",
-    version="0.0.7",
+    name="c-lock",
+    version="0.0.7.1",
+    description="",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.6",
     author="Javier Junquera Sánchez",
     author_email="javier@junquera.xyz",
-    packages=find_packages(),
+    # packages=find_packages(),
+    packages=["clock"],
     url="https://gitlab.com/junquera/c-lock",
-    license="LICENSE.txt",
+    license="MIT License",
     install_requires=requirements(),
     tests_require=["pytest"],
     entry_points=dict(
