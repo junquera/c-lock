bd75bda2048d55f12ebca1a13ea46029e9b083c7
Javier Junquera Sánchez <javier@junquera.xyz>
2019-04-04 08:58:27 +0200

    Big fix in ttp

--- clockngpn/ttp.py
+++ clockngpn/ttp.py
@@ -19,7 +19,7 @@ def gen_ports_from_pin(pin, n_ports):
 
     for i in range(n_ports):
         aux = totp.hotp(K=pin.to_bytes(3, byteorder='big'), I=i)
-        r = int(aux/1e6) * (max_port - min_port)
+        r = int(((aux/1e6) * (max_port - min_port)) + min_port)
 
         values.append(aux + r)
 
